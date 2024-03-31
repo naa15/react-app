f020562b97824a9ec2a39af2390ea7fe2c4b6cab
naa15 <naltu15@freeuni.edu.ge>
2024-04-01 02:48:57 +0400

    Added some changes with states

--- src/Content.js
+++ src/Content.js
@@ -1,42 +1,62 @@
-import searchIcon from './search-icon.svg';
-import macbook from './macbook.png';
+import searchIcon from "./search-icon.svg";
+import macbook from "./macbook.png";
+import { useState } from "react";
 
+const products = [
+  { name: "Product A", description: "Description", price: 20 },
+  { name: "Product B", description: "Description", price: 10 },
+  { name: "Product C", description: "Description", price: 30 },
+];
 
-export function Search () {
-    return (
-        <div className='searchBar'>
-            <input type="text" placeholder="Search" />
-            <img src={searchIcon} alt='search icon' />
-        </div>
-    );
+export function Search() {
+  const [products, setProducts] = useState([
+    { name: "Product A", description: "Description", price: 20 },
+    { name: "Product B", description: "Description", price: 10 },
+    { name: "Product C", description: "Description", price: 30 },
+  ]);
+
+  const handleClick = () => {
+    const sortedProducts = [...products].sort((a, b) => a.price - b.price);
+    console.log(sortedProducts);
+    setProducts(sortedProducts);
+  };
+
+  return (
+    <div className="searchBar">
+      <input type="text" placeholder="Search" />
+      {/* <img src={searchIcon} alt='search icon' /> */}
+      <button onClick={handleClick}>sort</button>
+    </div>
+  );
 }
 
-export function Product () {
-    return (
-        <div className='grid-item'>
-            <img alt='product' src={macbook} />
-            <h4>title</h4>
-            <p>description</p>
-            <button>Add to Cart</button>
-        </div>
-    );
+export function Product(props) {
+  return (
+    <div className="grid-item">
+      <img alt="product" src={macbook} />
+      <h4>title</h4>
+      <p>description</p>
+      <p>{props?.price}</p>
+      <button>Add to Cart</button>
+    </div>
+  );
 }
 
-export function Products () {
-    return (
-        <div className='grid'>
-            <Product/>
-            <Product/>
-            <Product/>
-        </div>
-    );
+export function Products() {
+  return (
+    <div className="grid">
+      {products.map((item) => (
+        <Product price={item.price} />
+      ))}
+    </div>
+  );
 }
 
-export function Content () {
-    return (
-        <main className='main'> 
-            <Search/>
-            <Products/>
-        </main>
-    );
+export function Content() {
+  return (
+    <main className="main">
+      <Search />
+      <Products />
+    </main>
+  );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,38 +1,50 @@
+import { useState } from "react";
+
 function Profile() {
-    return (
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const handleChange1 = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleChange2 = (e) => {
+    setNewPassword(e.target.value);
+  };
+
+
+  return (
+    <div>
       <div>
-        <div>
-          <p>
-            Name: <strong>John</strong>
-          </p>
-          <p>
-            Surename: <strong>Doe</strong>
-          </p>
-        </div>
         <p>
-          Email: <strong>John.Doe@Gmail.com</strong>
+          Name: <strong>John</strong>
         </p>
-        <form action="">
-          <input
-            type="password"
-            name="current"
-            placeholder="Password"
-            id=""
-          />
-          <input
-            type="password"
-            name="new"
-            placeholder="Confirm Password"
-            id=""
-          />
-          <button
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
+        <p>
+          Surename: <strong>Doe</strong>
+        </p>
       </div>
-    );
-  }
-   
-  export default Profile;
+      <p>
+        Email: <strong>John.Doe@Gmail.com</strong>
+      </p>
+      <form action="">
+        <input
+          type="password"
+          name="current"
+          placeholder="Password"
+          value={password}
+          onChange={handleChange1}
+        />
+        <input
+          type="password"
+          name="new"
+          placeholder="Confirm Password"
+          value={newPassword}
+          onChange={handleChange2}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Profile;
